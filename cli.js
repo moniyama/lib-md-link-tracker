850391ca2ef06cc757a1626fce27721c68f34334
Monica Yamazaki <monicasayama@gmail.com>
2019-12-02 10:32:19 -0300

    substituindo forEach por map

--- cli.js
+++ cli.js
@@ -2,16 +2,15 @@
 
 // linha para utilizar o bin no node.js que está no package.json
 //package.json: geralmente o name = comando excutável no bin
-    //package.json:
-    //echo é pra printar na tela
-    //'jest --coverage' mostra a tabela % do teste
-const mdlinks = require('./lib/index.js')
+//package.json:
+//echo é pra printar na tela
+//'jest --coverage' mostra a tabela % do teste
+const mdlinks = require("./lib/index.js");
 
 mdlinks(process.argv[2])
-    .then((result) => {
-        result.forEach(element => {
-            console.log(element.href + " " + element.text)
-        });
-        console.log(process.argv)
-    })
-    .catch((error) => console.log(error))
+  .then((result) => {
+    result.forEach(element => {
+      console.log(element.href + " " + element.text)
+    });
+  })
+  .catch((error) => console.log(error))
