7013141da70cd3aa3fbce6266d5030811387adbb
Monica Yamazaki <monicasayama@gmail.com>
2019-12-03 13:41:05 -0300

    rev print no console de erro

--- cli.js
+++ cli.js
@@ -12,14 +12,8 @@ mdlinks(process.argv[2])
   .then((result) => {
     Array.isArray(result)
       ? result.forEach(element => {
-        console.log(element.href + " " + element.text);
+        console.log(element.href, element.text);
       })
       : console.log("Não há links no arquivo");
   })
-  .catch((error) => { 
-    console.log("Ocorreu um erro:");
-    console.log("nº erro:", error.errno);
-    console.log("Código:", error.code);
-    console.log("Syscall:", error.syscall);
-    console.log("Path:", error.path);
-  });
+  .catch(() => { console.log("Ocorreu um erro: Arquivo/diretório não encontrado!");});
