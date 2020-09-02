b65c1a1159a4a98e225e2c28347785bc420a3c8b
Monica <monicasayama@gmail.com>
2020-09-02 20:38:33 -0300

    cli

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 
 const mdlinks = require("./lib/index.js");
 
-mdlinks(process.argv[2])
+mdlinks(process.argv[2], { validate: false })
 	.then((result) => {
 		result.forEach((element) => {
 			console.log(element.href, element.text.substring(0, 50));
